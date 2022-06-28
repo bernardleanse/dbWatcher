6bbcc024b26c1a8353579ddd6fe056544d2e6e4b
Bernard Leanse <bernardleanse96@gmail.com>
2022-06-28 15:49:47 +0100

    separated out creating all the connections

--- db_connections.py
+++ db_connections.py
@@ -5,8 +5,23 @@ class DatabaseConnections:
   def __init__(self):
     self.databases = json.load(open("databases.json"))
     self.connections = {}
-
+    self.username = "seta"
+    self.password = "defaultUnsafePassword"
+    
+# can make this faster by commenting out most of the json in the file.
   def set_up_connections(self):
-    pass
+    for cntry, host in self.databases.items():
+      self.create_connection(country=cntry, host=host)
 
-db_cons = DatabaseConnections()
+  def create_connection(self, **kwargs):
+    self.connections[kwargs["country"]] = pg.connect(
+        host=kwargs["host"],
+        database=kwargs["country"],
+        user=self.username,
+        password=self.password
+      )
+
+dbc = DatabaseConnections()
+dbc.set_up_connections()
+print(dbc.connections)
+
